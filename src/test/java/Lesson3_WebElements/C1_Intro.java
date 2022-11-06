10c9fb4cc4d2f882c765da51165713fd2d230741
didemcbay <didemcicekk@gmail.com>
2022-11-06 22:29:23 +0300

    Web Elements Locators

---
+++ src/test/java/Lesson3_WebElements/C1_Intro.java
@@ -0,0 +1,19 @@
+package Lesson3_WebElements;
+
+public class C1_Intro {
+/*
+● Web sitesinde gördügümüz her şey Web Elementidir.
+● Elementlere yaptırmak için elementin id veya name veya tagı unique olanını bulup drivera söylememiz lazım
+    aynı olanlardan olursa başka bir elementi veya elementin özelligini söylemeliyiz.
+● Biz bir web elementine ulaşmak istiyorsak onun unique adresini driverimiza söylemeliyiz.
+    Unique adresi inspect yapıp HTML kodlarından bulabiliriz.
+● Locater yer bulucudur , git şuraya şunu bul şuna bas dememize yarar, 8 tane lokater çeşidi vardır.
+● id - name - className - tagName - linkTest(a href) - partialLinkTest - xpath - cssSelector
+● driver.findElement (By.id("twotabsearchtextbox"));  -- Bunu bir variable'a assign etmeliyiz.
+● Yanlış id kullanımında - NoSuchElementException hatası verir.
+
+
+
+ */
+
+}
